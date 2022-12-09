83ac3d2b00bb31755f200867ef41a2c479e59bd5
anon8105 <114602720+anon8105@users.noreply.github.com>
2022-12-09 05:02:09 -0700

    Add files via upload

---
+++ automation.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+echo "You have been hacked."
+sleep 1
+echo "We have taken over all the computers on you network."
+sleep 1
+echo "All your files have been encrypted and are controlled by Zephyr."
+sleep 1
+echo "If you wish for your files to be decrypted, please contact this email."
+echo "zephyrresponseteam@mail.com"
+sleep 10
+echo "If you have fallen for this you need to have some lessons on computers this is not a virus, merely a bash script that says you have been hacked."
